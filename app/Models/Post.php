54aead1f085eb535f252360ac1f66b90199c9bec
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2021-02-13 01:40:43 -0500

    disenio de bbdd

---
+++ app/Models/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * Relacion uno a muchos inversa
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    /**
+     * Relacion uno a muchos inversa
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Relacion muchso a muchos
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    /**
+     * Relacion uno a uno poliformica
+     */
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
